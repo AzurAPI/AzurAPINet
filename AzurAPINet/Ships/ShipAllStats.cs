6cc6cc79bd0c797aeb6f2b4121a85c98517f67c8
Jan0660 <58996212+Jan0660@users.noreply.github.com>
2020-10-13 19:13:31 +0200

    ShipAllStats.ToList method

--- AzurAPINet/Ships/ShipAllStats.cs
+++ AzurAPINet/Ships/ShipAllStats.cs
@@ -19,5 +19,19 @@ namespace Jan0660.AzurAPINet.Ships
         public readonly ShipStats? Level100Retrofit;
         [JsonProperty("level120Retrofit")]
         public readonly ShipStats? Level120Retrofit;
+        /// <summary>
+        /// Gets all the available ShipStats to a list
+        /// </summary>
+        /// <returns></returns>
+        public List<ShipStats> ToList()
+        {
+            var list = new List<ShipStats>() { BaseStats, Level100, Level120 };
+            if(Level100Retrofit != null)
+            {
+                list.Add(Level100Retrofit);
+                list.Add(Level120Retrofit);
+            }
+            return list;
+        }
     }
 }
