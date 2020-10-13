5ab8260187502d16296118838a372a0d76435c42
Jan0660 <58996212+Jan0660@users.noreply.github.com>
2020-10-13 17:49:17 +0200

    add retrofit stats

--- AzurAPINet/Ships/ShipAllStats.cs
+++ AzurAPINet/Ships/ShipAllStats.cs
@@ -13,5 +13,9 @@ namespace Jan0660.AzurAPINet.Ships
         public readonly ShipStats Level100;
         [JsonProperty("level120")]
         public readonly ShipStats Level120;
+        [JsonProperty("level100Retrofit")]
+        public readonly ShipStats Level100Retrofit;
+        [JsonProperty("level120Retrofit")]
+        public readonly ShipStats Level120Retrofit;
     }
 }
