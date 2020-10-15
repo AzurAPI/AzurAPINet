e94a5aa46c05218924877a86f715fa3bf431f113
Jan0660 <58996212+Jan0660@users.noreply.github.com>
2020-10-15 20:18:56 +0200

    add names to ShipSkinInfo

--- AzurAPINet/Ships/ShipSkinInfo.cs
+++ AzurAPINet/Ships/ShipSkinInfo.cs
@@ -5,8 +5,25 @@ using System.Text;
 
 namespace Jan0660.AzurAPINet.Ships
 {
+#nullable enable
+#nullable disable warnings
     public class ShipSkinInfo
     {
+        /// <summary>
+        /// english skin name, present in non-default skins
+        /// </summary>
+        [JsonProperty]
+        public readonly string? enClient;
+        /// <summary>
+        /// japanese english skin name, present in non-default skins
+        /// </summary>
+        [JsonProperty]
+        public readonly string? jpClient;
+        /// <summary>
+        /// chinese skin name, present in non-default skins
+        /// </summary>
+        [JsonProperty]
+        public readonly string? cnClient;
         [JsonProperty("obtainedFrom")]
         public readonly string ObtainedFrom;
         [JsonProperty("live2dModel")]
