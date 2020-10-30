a32773accb6f22d005c2853f4fe35aaa92b47448
Jan0660 <58996212+Jan0660@users.noreply.github.com>
2020-10-30 11:32:29 +0100

    add ReloadXAsync unit tests

---
+++ AzurAPINetTests/ReloadXAsync.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static AzurAPINetTests.Static;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AzurAPINetTests
+{
+    [TestClass]
+    public class ReloadXAsync
+    {
+        [TestMethod]
+        public void ReloadShipsAsync()
+        {
+            Client.ReloadShipsAsync().Wait();
+        }
+        [TestMethod]
+        public void ReloadChaptersAsync()
+        {
+            Client.ReloadChaptersAsync().Wait();
+        }
+        [TestMethod]
+        public void ReloadEventsAsync()
+        {
+            Client.ReloadEventsAsync().Wait();
+        }
+        [TestMethod]
+        public void ReloadBarrageAsync()
+        {
+            Client.ReloadBarrageAsync().Wait();
+        }
+        [TestMethod]
+        public void ReloadMemoriesAsync()
+        {
+            Client.ReloadMemoriesAsync().Wait();
+        }
+        [TestMethod]
+        public void ReloadEquipmentAsync()
+        {
+            Client.ReloadEquipmentAsync().Wait();
+        }
+        [TestMethod]
+        public void ReloadVoiceLinesAsync()
+        {
+            Client.ReloadVoiceLinesAsync().Wait();
+        }
+    }
+}
