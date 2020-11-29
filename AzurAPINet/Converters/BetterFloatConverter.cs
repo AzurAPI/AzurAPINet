f0755da0dd08415048babb985dc8591cc155a676
Jan0660 <58996212+Jan0660@users.noreply.github.com>
2020-11-29 16:08:05 +0100

    remove debugging code

--- AzurAPINet/Converters/BetterFloatConverter.cs
+++ AzurAPINet/Converters/BetterFloatConverter.cs
@@ -13,30 +13,18 @@ namespace Jan0660.AzurAPINet.Converters
     /// </summary>
     public class BetterFloatConverter : JsonConverter<float>
     {
-        // pain
-        const string Numbers = "1234567890";
         // todo: theres probably a smarter way to do this
         public override float ReadJson(JsonReader reader, Type objectType, [AllowNull] float existingValue, bool hasExistingValue, JsonSerializer serializer)
         {
             string str = reader.Value.ToString();
             str = str.Replace("s", "").Replace("%", "");
-            try
-            {
-                /* it took me half a hour to realize that when you set localization to czech
-                 * or just set units to use what czech use
-                 * float.Parse("1.44") will die because we dont use . we use ,
-                 * half a hour...
-                 * god, please erase this country
-                */
-                return float.Parse(str, CultureInfo.InvariantCulture);
-                if (Numbers.Contains(str.Last()))
-                    return float.Parse(str);
-                else
-                    return float.Parse(str.Remove(str.Length - 1));
-            }catch(Exception e)
-            {
-                return 0.0f;
-            }
+            /* it took me half a hour to realize that when you set localization to czech
+             * or just set units to use what czech use
+             * float.Parse("1.44") will die because we dont use . we use ,
+             * half a hour...
+             * god, please erase this country
+            */
+            return float.Parse(str, CultureInfo.InvariantCulture);
         }
 
         public override void WriteJson(JsonWriter writer, [AllowNull] float value, JsonSerializer serializer)
